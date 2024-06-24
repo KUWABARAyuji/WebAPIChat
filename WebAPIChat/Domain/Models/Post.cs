f9fa4ea0a0fd55392ab81fad0e821a350ac7fe22
Enzo Yuji Kuwabara <enzo.kuwabara@7comm.com.br>
2024-06-23 21:07:12 -0300

    Initial commit

---
+++ WebAPIChat/Domain/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace MyWallWebAPI.Domain.Models
+{
+    public class Post
+    {
+
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime Data { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
